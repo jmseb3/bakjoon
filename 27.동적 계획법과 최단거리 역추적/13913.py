89ba1fa6b1d032487c9dd83af782555f39644ccc
jmseb3 <jmseb3@naver.com>
2021-08-31 17:20:53 +0900

    21/08/31

--- 27.동적 계획법과 최단거리 역추적/13913.py
+++ 27.동적 계획법과 최단거리 역추적/13913.py
@@ -3,20 +3,34 @@ from collections import deque
 N, K = map(int, input().split())
 
 MAX_NUM = 100_001
-dp = [[int(1e9), list()]] * (100_001)
-dp[5] = [0, [5]]
+move = [-1]*MAX_NUM
+path = []
 
 
 def possible(a):
-    if 0 <= a <= 100_001:
+    if 0 <= a < MAX_NUM:
         return True
     return False
 
 
-q = deque()
-q.append(N)
+def bfs(start):
+    q = deque([(start, 0)])
+    while q:
+        now, time = q.popleft()
+        if now == K:
+            tmp = now
+            while tmp != start:
+                path.append(tmp)
+                tmp = move[tmp]
+            path.append(start)
+            return time
 
-while q:
-    now = q.popleft()
-    if now == K:
-        print()
+        next = [now-1, now+1, now*2]
+        for x in next:
+            if possible(x) and move[x] == -1:
+                move[x] = now
+                q.append((x, time+1))
+
+
+print(bfs(N))
+print(*path[::-1])
